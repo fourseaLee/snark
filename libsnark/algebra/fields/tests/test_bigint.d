7ff8e62027f13bac78bdf09699ca47476d959500
fourseaLee <913357989@qq.com>
2019-05-23 20:29:41 +0800

    first commit

---
+++ libsnark/algebra/fields/tests/test_bigint.d
@@ -0,0 +1,5 @@
+libsnark/algebra/fields/tests/test_bigint.o: \
+ libsnark/algebra/fields/tests/test_bigint.cpp \
+ libsnark/algebra/fields/bigint.hpp libsnark/common/serialization.hpp \
+ libsnark/common/serialization.tcc libsnark/common/utils.hpp \
+ libsnark/common/utils.tcc libsnark/algebra/fields/bigint.tcc
---
+++ libsnark/algebra/fields/tests/test_fields.d
@@ -0,0 +1,25 @@
+libsnark/algebra/fields/tests/test_fields.o: \
+ libsnark/algebra/fields/tests/test_fields.cpp \
+ libsnark/common/profiling.hpp \
+ libsnark/algebra/curves/alt_bn128/alt_bn128_pp.hpp \
+ libsnark/algebra/curves/public_params.hpp \
+ libsnark/algebra/curves/alt_bn128/alt_bn128_init.hpp \
+ libsnark/algebra/fields/fp.hpp libsnark/algebra/fields/bigint.hpp \
+ libsnark/common/serialization.hpp libsnark/common/serialization.tcc \
+ libsnark/common/utils.hpp libsnark/common/utils.tcc \
+ libsnark/algebra/fields/bigint.tcc \
+ libsnark/algebra/exponentiation/exponentiation.hpp \
+ libsnark/algebra/exponentiation/exponentiation.tcc \
+ libsnark/algebra/fields/fp.tcc libsnark/algebra/fields/fp_aux.tcc \
+ libsnark/algebra/fields/field_utils.hpp \
+ libsnark/algebra/fields/field_utils.tcc \
+ libsnark/common/assert_except.hpp libsnark/algebra/fields/fp2.hpp \
+ libsnark/algebra/fields/fp2.tcc libsnark/algebra/fields/fp6_3over2.hpp \
+ libsnark/algebra/fields/fp6_3over2.tcc \
+ libsnark/algebra/fields/fp12_2over3over2.hpp \
+ libsnark/algebra/fields/fp12_2over3over2.tcc \
+ libsnark/algebra/curves/alt_bn128/alt_bn128_g1.hpp \
+ libsnark/algebra/curves/curve_utils.hpp \
+ libsnark/algebra/curves/curve_utils.tcc \
+ libsnark/algebra/curves/alt_bn128/alt_bn128_g2.hpp \
+ libsnark/algebra/curves/alt_bn128/alt_bn128_pairing.hpp
